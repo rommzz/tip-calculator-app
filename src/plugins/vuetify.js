346312de27a12325a88d9ab6174843932491ee83
rommzz <rommy_zohara@yahoo.com>
2021-07-27 09:09:20 +0700

    initial commit

---
+++ src/plugins/vuetify.js
@@ -0,0 +1,20 @@
+import Vue from 'vue';
+import Vuetify from 'vuetify/lib/framework';
+
+Vue.use(Vuetify);
+
+const vuetify = new Vuetify({
+  theme: {
+    themes: {
+      light: {
+        strongcyan: '#26c0ab',
+        verydarkcyan: '#00494d',
+        darkgrayishcyan: '#5e7a7d',
+        lightgrayishcyan: '#c5e4e7',
+
+      },
+    },
+  },
+})
+
+export default vuetify
